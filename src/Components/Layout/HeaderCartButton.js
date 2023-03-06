f908b61ccc8cc6fa943cc9092d255cbb76b6effd
Nick Kirui <nickkirui69@gmail.com>
2023-03-06 20:33:57 +0300

    Homepage

---
+++ src/Components/Layout/HeaderCartButton.js
@@ -0,0 +1,14 @@
+import classes from './HeaderCartButton.module.css'
+
+const HeaderCartButton = props => {
+    return <button className={classes.button} >
+        <span >
+        
+        </span>
+        <span>Your Cart</span>
+        <span className={classes.badge}>3 </span>
+
+    </button>
+}
+
+export default HeaderCartButton
---
+++ src/Components/Meals/MealsSummary.js
@@ -0,0 +1,10 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+
+    return <section>
+        
+    </section>
+};
+
+export default MealsSummary;
